5bae166536f67f760b7ea5c2ccc046c6b04bc3d0
Jo Matsumaru <hatunehimeka@outlook.jp>
2023-11-08 03:25:19 +0900

    V2 1

---
+++ EveryThing_Assist/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using Avalonia.Controls;
+
+namespace EveryThingAssistV2;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
